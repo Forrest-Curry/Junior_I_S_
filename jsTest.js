47b65f758a3433c82cac855c082bdbbe04837cc8
Forrest Curry <ecurry25@wooster.edu>
2024-02-20 16:51:41 -0500

    button drag test

--- jsTest.js
+++ jsTest.js
@@ -1,24 +1,20 @@
-// Step 1
-const slider = interact('.slider')    // target elements with the "slider" class
+import interact from "@interactjs/interactjs";
 
-slider
-  // Step 2
-  .draggable({                        // make the element fire drag events
-    origin: 'self',                   // (0, 0) will be the element's top-left
-    inertia: true,                    // start inertial movement if thrown
-    modifiers: [
-      interact.modifiers.restrict({
-        restriction: 'self'           // keep the drag coords within the element
-      })
-    ],
-    // Step 3
-    listeners: {
-      move (event) {                  // call this listener on every dragmove
-        const sliderWidth = interact.getElementRect(event.target).width
-        const value = event.pageX / sliderWidth
+const position = { x: 0, y: 0 }
 
-        event.target.style.paddingLeft = (value * 100) + '%'
-        event.target.setAttribute('data-value', value.toFixed(2))
-      }
-    }
-  })
+interact('.draggable').draggable({
+  listeners: {
+    start (event) {
+      console.log(event.type, event.target)
+    },
+
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+})
+
